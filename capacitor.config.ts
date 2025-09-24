f12d85a83d1116b45c04b29a82b0db55df7683bb
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-09-24 19:42:24 +0700

    add android and ios

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'io.github.hieudoanm.micro.saas',
+	appName: '@micro/saas',
+	webDir: 'docs',
+};
+
+export default config;
